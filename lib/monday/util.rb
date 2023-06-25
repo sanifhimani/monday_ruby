d9f0935a17a49ba809de4054e671c8879889a620
Sanif Himani <sanifhimani92@gmail.com>
2023-06-25 13:36:03 -0400

    Add formatting for multi-word items

--- lib/monday/util.rb
+++ lib/monday/util.rb
@@ -1,17 +1,33 @@
 # frozen_string_literal: true
 
 module Monday
-  module Util
-    def self.format_args(obj)
-      obj.map do |key, value|
-        "#{key}: #{value}"
-      end.join(", ")
-    end
+  class Util
+    class << self
+      def format_args(obj)
+        obj.map do |key, value|
+          "#{key}: #{formatted_args_value(value)}"
+        end.join(", ")
+      end
 
-    def self.format_select(array)
-      array.map do |item|
-        item.is_a?(Hash) ? "#{item.keys.first} { #{item.values.join(" ")} }" : item.to_s
-      end.join(" ")
+      def format_select(array)
+        array.map do |item|
+          item.is_a?(Hash) ? "#{item.keys.first} { #{item.values.join(" ")} }" : item.to_s
+        end.join(" ")
+      end
+
+      private
+
+      def single_word?(word)
+        return word unless word.is_a?(String)
+
+        !word.strip.include?(" ")
+      end
+
+      def formatted_args_value(value)
+        return "\"#{value}\"" unless single_word?(value)
+
+        value
+      end
     end
   end
 end
