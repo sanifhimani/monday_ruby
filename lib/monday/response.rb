0307c5acc011ec6f149ddb305c6f5268d7ccf962
Sanif Himani <sanifhimani92@gmail.com>
2023-06-25 13:36:44 -0400

    Update error handling

--- lib/monday/response.rb
+++ lib/monday/response.rb
@@ -6,20 +6,20 @@ module Monday
 
     def initialize(response)
       @response = response
-      @status = response.code.to_i
+      @status = errors? ? 400 : response.code.to_i
       @body = parse_body
       @headers = parse_headers
     end
 
     def success?
-      (200..299).cover?(status) && !error?
+      (200..299).cover?(status) && !errors?
     end
 
     private
 
     attr_reader :response
 
-    def error?
+    def errors?
       parse_body.key?("errors") || parse_body.key?("error_message")
     end
 
