65760e3a6f6133bcb51e658099d7cf116c9cf4b8
Sanif Himani <sanifhimani92@gmail.com>
2023-06-25 13:36:30 -0400

    Update request body

--- lib/monday/request.rb
+++ lib/monday/request.rb
@@ -2,11 +2,15 @@
 
 module Monday
   class Request
-    def self.post(uri, body, headers)
+    def self.post(uri, query, headers)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       request = Net::HTTP::Post.new(uri.request_uri, headers)
-      request.body = body.to_json
+
+      request.body = {
+        "query" => query
+      }.to_json
+
       http.request(request)
     end
   end
