f5b2cc85a309e18543c4a8ed121a8468deb25ac4
Sanif Himani <sanifhimani92@gmail.com>
2023-07-04 11:48:23 -0400

    chore: configure vcr and dotenv

--- Gemfile
+++ Gemfile
@@ -7,8 +7,10 @@ gemspec
 
 group :development do
   gem "byebug", "~> 11.1"
+  gem "dotenv", "~> 2.8"
   gem "rake", "~> 13.0"
   gem "rspec", "~> 3.0"
   gem "rubocop", "~> 1.21"
-  gem "webmock", "~>3.18"
+  gem "vcr", "~> 6.2"
+  gem "webmock", "~> 3.18"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,21 +2,33 @@
 
 require "monday_ruby"
 require "webmock/rspec"
+require "vcr"
+require "dotenv"
+
+Dotenv.load
 
 WebMock.disable_net_connect!(allow_localhost: true)
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
   config.color_mode = :on
   config.formatter = :documentation
   config.order = :random
+  config.warnings = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+VCR.configure do |config|
+  config.hook_into :webmock
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.ignore_localhost = true
+  config.default_cassette_options = { record: :none }
+
+  config.filter_sensitive_data("<TOKEN>") { ENV.fetch("token", nil) }
+  config.configure_rspec_metadata!
 end
 
 def fixture_path
@@ -25,6 +37,18 @@ end
 
 def fixture(file)
   File.new("#{fixture_path}/#{file}")
+end
+
+def invalid_client
+  Monday::Client.new(
+    token: "invalid_token"
+  )
+end
+
+def valid_client
+  Monday::Client.new(
+    token: ENV.fetch("token", nil)
+  )
 end
 
 def monday_url
