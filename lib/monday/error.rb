2768f26c6a04281577180fcceb64a2f68a8407e8
Sanif Himani <sanifhimani92@gmail.com>
2023-07-10 09:11:34 -0400

    chore: update error object with status code returned from response

--- lib/monday/error.rb
+++ lib/monday/error.rb
@@ -7,7 +7,7 @@ module Monday
     def initialize(message: nil, response: nil, code: nil)
       @response = response
       @message = error_message(message)
-      @code = code.nil? ? response&.status : code
+      @code = error_code(code)
 
       super(message)
     end
@@ -20,11 +20,23 @@ module Monday
 
     private
 
+    def error_code(code)
+      return code unless code.nil?
+
+      response_error_code.nil? ? response&.status : response_error_code
+    end
+
     def error_message(message)
       return response_error_message if message.nil?
       return message if response_error_message.nil?
 
       "#{message}: #{response_error_message}"
+    end
+
+    def response_error_code
+      return if response.nil?
+
+      response.body["status_code"]
     end
 
     def response_error_message
