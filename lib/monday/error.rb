f81c0acbae105dd9cc49d35648fa1cfdde209a9f
Sanif Himani <sanifhimani92@gmail.com>
2023-07-10 11:07:00 -0400

    docs: add docs for error class

--- lib/monday/error.rb
+++ lib/monday/error.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module Monday
+  # Monday::Error is the base error class from which other
+  # specific error classes are derived.
   class Error < StandardError
     attr_reader :response, :message, :code
 
@@ -46,21 +48,45 @@ module Monday
     end
   end
 
+  # InternalServerError is raised when the request returns
+  # a 500 status code.
   class InternalServerError < Error
   end
 
+  # AuthorizationError is raised when the request returns
+  # a 401 or 403 status code.
+  #
+  # It is also raised when the body returns the following error_code:
+  #   UserUnauthorizedException
   class AuthorizationError < Error
   end
 
+  # RateLimitError is raised when the request returns
+  # a 429 status code.
   class RateLimitError < Error
   end
 
+  # ResourceNotFoundError is raised when the request returns
+  # a 404 status code.
+  #
+  # It is also raised when the body returns the following error_code:
+  #   ResourceNotFoundException
   class ResourceNotFoundError < Error
   end
 
+  # ResourceNotFoundError is raised when the request returns
+  # a 400 status code.
+  #
+  # It is also raised when the body returns the following error_codes:
+  #   InvalidUserIdException, InvalidVersionException, InvalidColumnIdException
+  #   InvalidItemIdException, InvalidBoardIdException, InvalidArgumentException
+  #   CreateBoardException, ItemsLimitationException, ItemNameTooLongException
+  #   ColumnValueException, CorrectedValueException
   class InvalidRequestError < Error
   end
 
+  # ComplexityError is raised when the body returns the following error_code:
+  #   ComplexityException
   class ComplexityError < Error
   end
 end
