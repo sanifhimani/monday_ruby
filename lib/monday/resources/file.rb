86061e00d3efe41a5faa7ee9cfb4cba4394fe9f4
Wes Hays <weshays@gmail.com>
2025-11-17 17:58:36 -0800

    Updated comments for File resource

--- lib/monday/resources/file.rb
+++ lib/monday/resources/file.rb
@@ -6,11 +6,11 @@ module Monday
     class File < Base
       DEFAULT_SELECT = %w[id].freeze
 
-      # Adds a file to an update for an item.
+      # Adds a file to a file type column for an item.
       #
-      # Allows customizing the update creation using the args option.
+      # Allows customizing the column update using the args option.
       # Allows customizing the values to retrieve using the select option.
-      # By default, ID, body and created_at fields are retrieved.
+      # By default, The ID is retrieved.
       def add_file_to_column(args: {}, select: DEFAULT_SELECT)
         cloned_args = args.clone
         variables = { file: cloned_args.delete(:file) }
@@ -23,8 +23,8 @@ module Monday
       #
       # Allows customizing the update creation using the args option.
       # Allows customizing the values to retrieve using the select option.
-      # By default, ID, body and created_at fields are retrieved.
-      def add_file_to_update(args: {}, select: [:id])
+      # By default, The ID is retrieved.
+      def add_file_to_update(args: {}, select: DEFAULT_SELECT)
         cloned_args = args.clone
         variables = { file: cloned_args.delete(:file) }
         cloned_args.merge!(file: '$file')
@@ -37,7 +37,7 @@ module Monday
       # Allows customizing the update creation using the args option.
       # Allows customizing the values to retrieve using the select option.
       # By default, ID is retrieved.
-      def clear_file_column(args: {}, select: %w[id])
+      def clear_file_column(args: {}, select: DEFAULT_SELECT)
         merged_args = args.merge(value: '{\"clear_all\": true}')
         query = "mutation { change_column_value#{Util.format_args(merged_args)} {#{Util.format_select(select)}}}"
         make_request(query)
